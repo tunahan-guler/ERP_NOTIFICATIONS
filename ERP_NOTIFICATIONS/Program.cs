0de53581984a189d6717ca1c7f15c2930312e1b0
Tunahan <tguler@mscteknoloji.com>
2023-07-28 15:18:14 +0300

    Add project files.

---
+++ ERP_NOTIFICATIONS/Program.cs
@@ -0,0 +1,29 @@
+using ERP_NOTIFICATIONS;
+
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions() { EnvironmentName = "Development", ApplicationName = "ERP_NOTIFICATIONS" });
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHostedService<TimeHostedService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ ERP_NOTIFICATIONS/TimeHostedService.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+
+namespace ERP_NOTIFICATIONS
+{
+    public class TimeHostedService : IHostedService
+    {
+
+        private readonly IServiceScopeFactory scopeFactory;
+        private Timer _timer;
+
+        public TimeHostedService(IServiceScopeFactory scopeFactory)
+        {
+            this.scopeFactory = scopeFactory;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            TimeSpan interval = new TimeSpan(0,0,5);
+
+            _timer = new Timer(DoWorkCodlean, null, TimeSpan.Zero, interval);
+
+            return Task.CompletedTask;
+        }
+        private async void DoWorkCodlean(object? state)
+        {
+            Console.WriteLine("Çalıştım");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
